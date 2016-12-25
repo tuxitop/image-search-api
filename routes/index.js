b60d3065c2785ab3616b0799417182c4ceac7bce
Ali Mousavi <ali.mousavi@gmail.com>
2016-12-25 15:48:42 +0330

    Initialized app and its homepage

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+/* jshint esversion: 6 */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Tuxitop URL shortner API', baseURL: req.get('host') });
+});
+
+module.exports = router;
